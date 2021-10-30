8427c041511b66215b5aa6a23cd45dcc2b7a880a
Victor Hugo Estrada Ortega <victorhugofp11@hotmail.com>
2021-10-29 22:45:49 -0600

    Proyecto subido

---
+++ generador de numeros/src/generador/de/numeros/Nodo.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generador.de.numeros;
+
+/**
+ *
+ * @author victo
+ */
+/**
+ * Clase que define los elementos que debe tener un Nodo de la lista.
+ * @author xavier
+ */
+public class Nodo {
+    // Variable en la cual se va a guardar el valor.
+    private int valor;
+    // Variable para enlazar los nodos.
+    private Nodo siguiente;
+    /**
+     * Constructor que inicializamos el valor de las variables.
+     */
+    public void Nodo(){
+        this.valor = 0;
+        this.siguiente = null;
+    }
+    
+    // Métodos get y set para los atributos.
+    
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }   
+}
